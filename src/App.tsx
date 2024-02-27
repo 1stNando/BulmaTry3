0568dedbd83e820121dcb83a06a840babd60c54d
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-02-26 22:59:36 -0500

    Added elements for container to practice spacing.

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,29 @@ export function App() {
         <a className="button is-link">Link</a>
       </div>
 
-      <div>Hello, World!!! </div>
+      <h1 className="py-6 my-2">Hello, Bulma</h1>
+      <h1 className="px-4 mx-6 py-4">Hello, Bulma</h1>
+      <h1 className="mt-4 mr-4 pb-6 pl-3">Hello, Bulma</h1>
+
+      <section className="section">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+          reprehenderit quasi voluptatibus sed error vero reiciendis aspernatur
+          nemo quas corporis aliquid impedit veniam, ipsa corrupti laudantium
+          laboriosam facere? Maiores, debitis.
+        </p>
+      </section>
+
+      <div className="section">
+        <div className="container">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+          eligendi tempore ea sunt rerum natus earum quia laborum excepturi
+          distinctio vero est aspernatur odit, quod maiores qui sint odio
+          accusantium!
+        </div>
+      </div>
+
+      <br></br>
       <h1 className="is-size-1 has-text-danger">first heading</h1>
       <h2 className="is-size-2 has-text-primary">second heading</h2>
       <h3 className="is-size-3 has-text-success">third heading</h3>
@@ -28,7 +50,7 @@ export function App() {
 
       <br></br>
 
-      <p className="has-text-primary has-background-light">I'm primary text</p>
+      {/* <p className="has-text-primary has-background-light">I'm primary text</p>
       <p className="has-text-warning has-background-danger">I'm warning text</p>
       <p className="has-text-danger-light">I'm danger text</p>
       <p className="has-text-info">I'm info text</p>
@@ -36,7 +58,7 @@ export function App() {
       <p className="has-text-dark">I'm dark text</p>
       <p className="has-text-light has-background-primary-dark">
         I'm light text
-      </p>
+      </p> */}
     </>
   )
 }
