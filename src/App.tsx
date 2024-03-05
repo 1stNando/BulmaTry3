ac55221a0e91ebc2ae421ca0060e4f64b075a183
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-03-05 18:55:40 -0500

    Up to video #7 which include a grid system and header of the page.

--- src/App.tsx
+++ src/App.tsx
@@ -3,62 +3,74 @@ import React from 'react'
 export function App() {
   return (
     <>
-      <nav>
-        <button className="button">Button</button>
-        <button className="button is-primary">Button</button>
-        <button className="button is-large">Button</button>
-        <button className="button is-light">Light</button>
-        <button className="button is-dark">Dark</button>
-        <button className="button is-black">Black</button>
+      <nav className="navbar has-shadow is-white">
+        <div className="navbar-brand">
+          <a className="navbar-item">
+            <img
+              src="src/assets/logo.png"
+              alt="site logo"
+              className="py-2 px-2"
+              max-height="100px"
+            />
+          </a>
+          <a className="navbar-burger" id="burger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </div>
+
+        <div className="navbar-menu" id="nav-links">
+          <div className="navbar-end">
+            <a className="navbar-item">My Account</a>
+            <a className="navbar-item">Shopping Cart ()</a>
+          </div>
+        </div>
       </nav>
 
-      <h1 className="title">Bulma</h1>
-      <div className="buttons">
-        <a className="button is-primary">Primary</a>
-        <a className="button is-link">Link</a>
+      <div className="section pt-4 pb-0">
+        <nav className="breadcrumb has-arrow-separator">
+          <ul className="container">
+            <li>
+              <a className="has-text-grey">Coffee Ninja</a>
+            </li>
+            <li>
+              <a>Shop</a>
+            </li>
+            <li className="is-active">
+              <a className="has-text-grey">Docker Dark Roast</a>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      <h1 className="py-6 my-2">Hello, Bulma</h1>
-      <h1 className="px-4 mx-6 py-4">Hello, Bulma</h1>
-      <h1 className="mt-4 mr-4 pb-6 pl-3">Hello, Bulma</h1>
-
+      {/* Product page below! */}
       <section className="section">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
-          reprehenderit quasi voluptatibus sed error vero reiciendis aspernatur
-          nemo quas corporis aliquid impedit veniam, ipsa corrupti laudantium
-          laboriosam facere? Maiores, debitis.
-        </p>
-      </section>
-
-      <div className="section">
         <div className="container">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-          eligendi tempore ea sunt rerum natus earum quia laborum excepturi
-          distinctio vero est aspernatur odit, quod maiores qui sint odio
-          accusantium!
+          <div className="columns is-vcentered">
+            <div className="column is-3">
+              <h1 className="is-size-1 title">Docker</h1>
+              <h2 className="is-size-2 subtitle">Dark Roast</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus possimus nam facilis esse laboriosam numquam eveniet
+                laborum est, perferendis natus labore.
+              </p>
+            </div>
+            <div className="column is-5 has-text-centered">
+              <img src="src/assets/product.png" className="px-6"></img>
+            </div>
+            <div className="column is-4">
+              <div className="is-size-4 mb-4">$15.99</div>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+                ipsum dolor sit, amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <br></br>
-      <h1 className="is-size-1 has-text-danger">first heading</h1>
-      <h2 className="is-size-2 has-text-primary">second heading</h2>
-      <h3 className="is-size-3 has-text-success">third heading</h3>
-      <h4 className="is-size-4 has-text-info">fouth heading</h4>
-      <h5 className="is-size-5 has-text-dark">fifth heading</h5>
-      <h6 className="is-size-6 has-text-warning">sixth heading</h6>
-
-      <br></br>
-
-      {/* <p className="has-text-primary has-background-light">I'm primary text</p>
-      <p className="has-text-warning has-background-danger">I'm warning text</p>
-      <p className="has-text-danger-light">I'm danger text</p>
-      <p className="has-text-info">I'm info text</p>
-      <p className="has-text-success">I'm success text</p>
-      <p className="has-text-dark">I'm dark text</p>
-      <p className="has-text-light has-background-primary-dark">
-        I'm light text
-      </p> */}
+      </section>
+      <script src="index.tsx"></script>
     </>
   )
 }
---
+++ src/components/pageOne.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export function FirstPage() {
+  return (
+    <>
+      <nav>
+        <button className="button">Button</button>
+        <button className="button is-primary">Button</button>
+        <button className="button is-large">Button</button>
+        <button className="button is-light">Light</button>
+        <button className="button is-dark">Dark</button>
+        <button className="button is-black">Black</button>
+      </nav>
+
+      <h1 className="title">Bulma</h1>
+      <div className="buttons">
+        <a className="button is-primary">Primary</a>
+        <a className="button is-link">Link</a>
+      </div>
+
+      <h1 className="py-6 my-2">Hello, Bulma</h1>
+      <h1 className="px-4 mx-6 py-4">Hello, Bulma</h1>
+      <h1 className="mt-4 mr-4 pb-6 pl-3">Hello, Bulma</h1>
+
+      <section className="section">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+          reprehenderit quasi voluptatibus sed error vero reiciendis aspernatur
+          nemo quas corporis aliquid impedit veniam, ipsa corrupti laudantium
+          laboriosam facere? Maiores, debitis.
+        </p>
+      </section>
+
+      <div className="section">
+        <div className="container">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+          eligendi tempore ea sunt rerum natus earum quia laborum excepturi
+          distinctio vero est aspernatur odit, quod maiores qui sint odio
+          accusantium!
+        </div>
+      </div>
+
+      <br></br>
+      <h1 className="is-size-1 has-text-danger">first heading</h1>
+      <h2 className="is-size-2 has-text-primary">second heading</h2>
+      <h3 className="is-size-3 has-text-success">third heading</h3>
+      <h4 className="is-size-4 has-text-info">fouth heading</h4>
+      <h5 className="is-size-5 has-text-dark">fifth heading</h5>
+      <h6 className="is-size-6 has-text-warning">sixth heading</h6>
+
+      <br></br>
+
+      {/* <p className="has-text-primary has-background-light">I'm primary text</p>
+      <p className="has-text-warning has-background-danger">I'm warning text</p>
+      <p className="has-text-danger-light">I'm danger text</p>
+      <p className="has-text-info">I'm info text</p>
+      <p className="has-text-success">I'm success text</p>
+      <p className="has-text-dark">I'm dark text</p>
+      <p className="has-text-light has-background-primary-dark">
+        I'm light text
+      </p> */}
+    </>
+  )
+}
