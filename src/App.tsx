b7ab9780fcb17efdf7f1d9b0c354f4ac933c08f7
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-03-05 19:09:56 -0500

    Completed video #8 - Responsive Classes. We adjusted sizes depending on  the screen of viewing. Mobile, tablet, desktop.

--- src/App.tsx
+++ src/App.tsx
@@ -47,20 +47,24 @@ export function App() {
       {/* Product page below! */}
       <section className="section">
         <div className="container">
-          <div className="columns is-vcentered">
-            <div className="column is-3">
-              <h1 className="is-size-1 title">Docker</h1>
-              <h2 className="is-size-2 subtitle">Dark Roast</h2>
+          <div className="columns is-vcentered is-multiline">
+            <div className="column is-6-tablet is-3-desktop">
+              <h1 className="is-size-3-mobile is-size-1-desktop title">
+                Docker
+              </h1>
+              <h2 className="is-size-3-mobile is-size-2-desktop subtitle">
+                Dark Roast
+              </h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Delectus possimus nam facilis esse laboriosam numquam eveniet
                 laborum est, perferendis natus labore.
               </p>
             </div>
-            <div className="column is-5 has-text-centered">
+            <div className="column is-6-tablet is-5-desktop has-text-centered">
               <img src="src/assets/product.png" className="px-6"></img>
             </div>
-            <div className="column is-4">
+            <div className="column is-12-tablet is-4-desktop">
               <div className="is-size-4 mb-4">$15.99</div>
               <p className="mb-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
@@ -70,6 +74,30 @@ export function App() {
           </div>
         </div>
       </section>
+
+      <section className="section">
+        <div className="container">
+          <div className="columns is-8 is-variable">
+            <div className="column is-7-tablet">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aspernatur enim pariatur recusandae voluptatibus voluptatum.
+                Fuga praesentium mollitia officiis quo velit ab aliquam
+                adipisci,
+              </p>
+            </div>
+            <div className="colum is-5-tablet">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                nisi. Non reiciendis enim possimus delectus eaque magnam quae
+                officia officiis, molestiae illum porro, qui odit, voluptatem
+                atque est repellendus dicta.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <script src="index.tsx"></script>
     </>
   )
