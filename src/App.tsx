8fb92cf9167104cd438553e47761576bdd7bff23
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-03-11 22:09:16 -0400

    Finished video #12 - Cards

--- src/App.tsx
+++ src/App.tsx
@@ -167,6 +167,72 @@ export function App() {
           </div>
         </div>
       </section>
+
+      {/* cards */}
+
+      <section className="section is-hidden-mobile">
+        <div className="container">
+          <h3 className="title has-text-centered is-size-4">
+            Related Products
+          </h3>
+          <div className="columns mt-5 is-8 is-variable is-centered">
+            <div className="column is-4-tablet is-3-desktop">
+              <div className="card">
+                <div className="card-image has-text-centered px-6">
+                  <img src="src/assets/p1.png" alt="product" />
+                </div>
+                <div className="card-content">
+                  <p>$12.95</p>
+                  <p className="title is-size-5">Cortado Cup</p>
+                </div>
+                <footer className="card-footer">
+                  <p className="card-footer-item">
+                    <a href="" className="has-text-grey">
+                      View
+                    </a>
+                  </p>
+                </footer>
+              </div>
+            </div>
+            <div className="column is-4-tablet is-3-desktop">
+              <div className="card">
+                <div className="card-image has-text-centered px-6">
+                  <img src="src/assets/p2.png" alt="product" />
+                </div>
+                <div className="card-content">
+                  <p>$12.95</p>
+                  <p className="title is-size-5">Docker Light</p>
+                </div>
+                <footer className="card-footer">
+                  <p className="card-footer-item">
+                    <a href="" className="has-text-grey">
+                      View
+                    </a>
+                  </p>
+                </footer>
+              </div>
+            </div>
+            <div className="column is-4-tablet is-3-desktop">
+              <div className="card">
+                <div className="card-image has-text-centered px-6">
+                  <img src="src/assets/p3.png" alt="product" />
+                </div>
+                <div className="card-content">
+                  <p>$12.95</p>
+                  <p className="title is-size-5">Coffee Crush</p>
+                </div>
+                <footer className="card-footer">
+                  <p className="card-footer-item">
+                    <a href="" className="has-text-grey">
+                      View
+                    </a>
+                  </p>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
